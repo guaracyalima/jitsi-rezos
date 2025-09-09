1e2ef2298149eba2224cea7f03345c76faba236f
Guaracy A. Lima <guaracyaraujolima@gmail.com>
2025-09-09 14:50:15 -0300

    Config Jitsi for Railway

---
+++ web/config.js
@@ -0,0 +1,8 @@
+var config = {
+  hosts: {
+    domain: 'meet.rezos.com.br',
+    muc: 'muc.meet.rezos.com.br',
+    anonymousdomain: 'guest.meet.rezos.com.br'
+  },
+  requireRoomPassword: true
+};
